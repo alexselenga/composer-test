fb9a4e5f2c434b81a55a74a223cd591b8b313fce
Михеев Алексей <alexselenga@yandex.ru>
2019-04-16 20:02:42 +0400

    Composer Package Example Test

---
+++ views/site/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use alexselenga\exampleWidget\Widget;
+
+echo Widget::widget([
+    'name' => 'redactor',
+    'settings' => [
+        'lang' => 'ru',
+        'minHeight' => 200,
+        'plugins' => [
+            'clips',
+            'fullscreen',
+        ],
+        'clips' => [
+            ['Lorem ipsum...', 'Lorem...'],
+            ['red', '<span class="label-red">red</span>'],
+            ['green', '<span class="label-green">green</span>'],
+            ['blue', '<span class="label-blue">blue</span>'],
+        ],
+    ],
+]);
